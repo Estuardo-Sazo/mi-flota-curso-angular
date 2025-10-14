7faf62f9b8177e8250581982bcb87cd55d2b2508
Jaime Estuardo Sazo <estuardosazo1206@gmail.com>
2025-10-13 21:39:39 -0600

    feat: add initial components and layouts for the application

---
+++ src/app/shared/components/add-vehicle-modal/add-vehicle-modal.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddVehicleModal } from './add-vehicle-modal';
+
+describe('AddVehicleModal', () => {
+  let component: AddVehicleModal;
+  let fixture: ComponentFixture<AddVehicleModal>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddVehicleModal]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddVehicleModal);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
