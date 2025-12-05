006b692833f9af71360855d3bdca2cedcb21a045
Jaime Estuardo Sazo <estuardosazo1206@gmail.com>
2025-12-05 16:42:04 -0600

    feat: implement initial Angular application structure with routing and a dynamic header navigation component.

--- src/app/shared/components/header-nav/header-nav.ts
+++ src/app/shared/components/header-nav/header-nav.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header-nav',
@@ -6,4 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './header-nav.html',
   styleUrl: './header-nav.css',
 })
-export class HeaderNav {}
+export class HeaderNav implements OnInit {
+  private router = inject(Router);
+
+  currentTitle = signal('Dashboard');
+
+  private routerTitles: { [key: string]: string } = {
+    '': 'Dashboard',
+    dashboard: 'Dashboard',
+    vehicles: 'Vehículos',
+    records: 'Transacciones',
+  };
+
+  ngOnInit() {
+    this.updateTitle(this.router.url);
+
+    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((e) => {
+      this.updateTitle(e.urlAfterRedirects);
+    });
+  }
+
+  private updateTitle(url: string) {
+    const route = url.split('/')[1];
+
+    const title = this.routerTitles[route] || 'Flota';
+    this.currentTitle.set(title);
+  }
+}
